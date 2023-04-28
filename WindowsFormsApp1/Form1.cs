b8bc8667815e95445f790999610a4ad00a183439
Student <Student@C208-20>
2023-04-28 11:37:13 +0200

    Nothing

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Plansza myGame;
+        private Graphics g;
+        private const int filedSize= 25;
+
+
+        public Form1()
+        { 
+        InitializeComponent();
+            myGame = new Plansza(new Size(10, 10));
+            pictureBoxVisualization.Image = new Bitmap(myGame.size.Width * filedSize,
+                                                     myGame.size.Height * filedSize);
+            g = Graphics.FromImage(pictureBoxVisualization.Image);
+
+            DrawBoard();
+        }
+
+        private void DrawBoard()
+        {
+            for(int col=0;col<myGame.size.Height;col++)
+            {
+                for (int row = 0; row < myGame.size.Height; row++)
+                    {
+                    g.DrawRectangle(new Pen(Color.Black), col * filedSize, row * filedSize, filedSize, filedSize);
+                    }
+            }
+            
+
+            //throw new NotImplementedException();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/Plansza.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class Plansza
+    {
+        int punkty;
+        Point aplce;
+        
+        Timer timer;
+       
+        
+        public Size size { get; }
+  
+
+        public Plansza(Size s)
+        {
+           
+            size = s;
+            List<Point> tmp = new List<Point>();
+            tmp.Add(new Point(size.Width / 2, size.Height / 2));
+        }
+    }
+    class Snake
+    {
+        public List<Point> segments;
+        public Snake(List<Point>Startposition)
+        {
+            segments = Startposition;
+        }
+    }
+}
